a33b1b4fc4b37be2abb9a35ec09747529009b8b4
peetonn <gpeetonn@gmail.com>
2017-10-30 17:06:46 -0700

    added test

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from nanomsg import *
+import _nanomsg_ctypes as nnc
+
+with Socket(SUB) as s:
+	s.connect("ipc:///tmp/testnano")
+	s.set_string_option(SUB, SUB_SUBSCRIBE, '')
+	frameSize = 324*240*4
+	while True:
+		nnc.nn_recvmsg(s, [4, frameSize])
+		print "frameNo "+ str(frameNo) + " and frame data (" + len(frame) + " bytes) received"
